a75b024e5bd03802636245143b5810a5115d04f6
Gleb Laychenkov <racers2no95@mail.ru>
2024-03-24 11:00:27 +0400

    Initial commit2

---
+++ src/main/java/ru/netology/javaqa/MoviePoster.java
@@ -0,0 +1,12 @@
+package ru.netology.javaqa;
+
+public class {
+
+    public PurchaseItem(int id, int productId, String productName, int productPrice, int count) {
+        this.id = id;
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.count = count;
+    }
+}
